4a7ab6e79d67f3c6f14e828090111e10698bfe27
AhammedNibras <103364567+AhammedNibras@users.noreply.github.com>
2024-05-18 21:43:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "examples" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
 
@@ -23,5 +23,5 @@ resource "aws_instance" "example" {
 }
 
 output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+  value = aws_instance.examples.public_ip
 }
