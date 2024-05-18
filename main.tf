b45d1f5e08f479cdc3bfb3a27ba7653b05a2ed86
AhammedNibras <103364567+AhammedNibras@users.noreply.github.com>
2024-05-18 22:47:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,40 +2,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_key_pair" "deployer_key" {
-  key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
-resource "aws_instance" "examples" {
-  ami           = "ami-04b70fa74e45c3917"
+resource "aws_instance" "app_instance" {
+  ami           = "ami-04b70fa74e45c3917" # Ubuntu AMI
   instance_type = "t2.micro"
 
-connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.public_ip
+  user_data = <<-EOF
+              #!/bin/bash
+              #curl -sL https://deb.nodesource.com/setup_14.x | bash -
+              sudo apt install -y nodejs
+              sudo apt install -y git
+              git clone https://github.com/<your-username>/hello-world-nodejs.git
+              cd hello-world-nodejs
+              npm install
+              npm start &
+              EOF
 
-    }
-
-provisioner "remote-exec" {
-    inline = [
-              "echo 'Hello from the remote instance'",
-              "sudo apt-get install -y nodejs",
-              "sudo apt-get install -y git",
-              "git clone https://github.com/AhammedNibras/hello-world-nodejs.git",
-              "cd hello-world-nodejs",
-              "npm install",
-              "npm start"
- ]
-}
-
-tags = {
+  tags = {
     Name = "HelloWorldAppInstance"
   }
 }
 
 output "instance_public_ip" {
-  value = aws_instance.examples.public_ip
+  value = aws_instance.app_instance.public_ip
 }
+
