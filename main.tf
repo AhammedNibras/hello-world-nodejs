5fd1d65a753d3b784191840f2577fe6fc98c9265
AhammedNibras <103364567+AhammedNibras@users.noreply.github.com>
2024-05-18 22:51:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ resource "aws_instance" "app_instance" {
   user_data = <<-EOF
               #!/bin/bash
               #curl -sL https://deb.nodesource.com/setup_14.x | bash -
-              sudo apt install -y nodejs
-              sudo apt install -y git
-              git clone https://github.com/<your-username>/hello-world-nodejs.git
+              sudo apt install nodejs -y
+              sudo apt install git -y
+              git clone https://github.com/AhammedNibras/hello-world-nodejs.git
               cd hello-world-nodejs
               npm install
               npm start &
