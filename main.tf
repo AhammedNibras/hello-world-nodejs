371b647282d66186ac49a816ec965b66cc3e74df
AhammedNibras <103364567+AhammedNibras@users.noreply.github.com>
2024-05-19 16:04:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -113,9 +113,3 @@ resource "aws_instance" "example" {
 output "instance_public_ip" {
   value = aws_instance.example.public_ip
 }
-
-
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
-}
-
