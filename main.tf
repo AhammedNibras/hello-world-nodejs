038e288810aa3802381c1de13324d063fa058aba
AhammedNibras <103364567+AhammedNibras@users.noreply.github.com>
2024-05-19 16:04:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,41 +2,118 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
 resource "aws_key_pair" "example" {
   key_name   = "terraform-demo"  
-  public_key = file("/home/codespace/.ssh/id_rsa.pub") 
+  public_key = file("~/.ssh/id_rsa.pub") 
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-sg"
+  }
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-04b70fa74e45c3917" # Ubuntu AMI
+  ami           = "ami-04b70fa74e45c3917" # Ubuntu AMI ami-04b70fa74e45c3917
   instance_type = "t2.micro"
   key_name      = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
 
  connection {
     type        = "ssh"
     user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = file("/home/codespace/.ssh/id_rsa")  # Replace with the path to your private key
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
     host        = self.public_ip
   }
 
   # File provisioner to copy a file from local to the remote EC2 instance
   provisioner "file" {
     source      = "index.js"  # Replace with the path to your local file
-    destination = "/home/ubuntu/hello-world-nodejs/index.js"  # Replace with the path on the remote instance
+    destination = "/home/ubuntu/index.js"  # Replace with the path on the remote instance
+  }
+
+  provisioner "file" {
+    source      = "package.json"  # Replace with the path to your local file
+    destination = "/home/ubuntu/package.json"  # Replace with the path on the remote instance
   }
 
   provisioner "remote-exec" {
     inline = [
-              "curl -sL https://deb.nodesource.com/setup_14.x | bash -",
+              "echo 'hello from remote instance'",
+              "sudo apt update -y",
               "sudo apt install nodejs -y",
+              "sudo apt install npm -y",
               "sudo apt install git -y",
-              #"git clone https://github.com/AhammedNibras/hello-world-nodejs.git",
-              "cd /home/ubuntu/hello-world-nodejs",
+              "cd /home/ubuntu",
               "npm install",
               "npm start",
-]
+            ]
+  }
 }
+
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
 }
+
 
 output "instance_public_ip" {
   value = aws_instance.example.public_ip
