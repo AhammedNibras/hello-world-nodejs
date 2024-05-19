8da86b6deb21623141b93e19e068ca2b9d2ea352
AhammedNibras <103364567+AhammedNibras@users.noreply.github.com>
2024-05-19 11:12:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,24 +2,40 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_key_pair" "example" {
+  key_name   = "terraform-demo"  
+  public_key = file("~/.ssh/id_rsa.pub") 
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-04b70fa74e45c3917" # Ubuntu AMI
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.example.key_name
 
-  user_data = <<-EOF
-              #!/bin/bash
-              #curl -sL https://deb.nodesource.com/setup_14.x | bash -
-              sudo apt install nodejs -y
-              sudo apt install git -y
-              git clone https://github.com/AhammedNibras/hello-world-nodejs.git
-              cd hello-world-nodejs
-              npm install
-              npm start &
-              EOF
-
-  tags = {
-    Name = "HelloWorldAppInstance"
+ connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    host        = self.public_ip
   }
+
+  # File provisioner to copy a file from local to the remote EC2 instance
+  provisioner "file" {
+    source      = "index.js"  # Replace with the path to your local file
+    destination = "/home/ubuntu/hello-world-nodejs/index.js"  # Replace with the path on the remote instance
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+              "curl -sL https://deb.nodesource.com/setup_14.x | bash -",
+              "sudo apt install nodejs -y",
+              "sudo apt install git -y",
+              #"git clone https://github.com/AhammedNibras/hello-world-nodejs.git",
+              "cd /home/ubuntu/hello-world-nodejs",
+              "npm install",
+              "npm start",
+]
+}
 }
 
 output "instance_public_ip" {
