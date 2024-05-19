e4faa23d39587355e25cd0578dbb081f73c2d6c1
AhammedNibras <103364567+AhammedNibras@users.noreply.github.com>
2024-05-19 11:14:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_key_pair" "example" {
   key_name   = "terraform-demo"  
-  public_key = file("~/.ssh/id_rsa.pub") 
+  public_key = file("/home/codespace/.ssh/id_rsa.pub") 
 }
 
 resource "aws_instance" "example" {
@@ -15,7 +15,7 @@ resource "aws_instance" "example" {
  connection {
     type        = "ssh"
     user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    private_key = file("/home/codespace/.ssh/id_rsa")  # Replace with the path to your private key
     host        = self.public_ip
   }
 
